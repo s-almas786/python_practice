bfc26ceeaaff086fc0a5cf84443f528bcacf91a2
Samar Almas <samaralmas8@gmail.com>
2022-03-15 18:02:09 +0500

    Lecture 2

---
+++ lecture_2.py
@@ -0,0 +1,29 @@
+# name = "Samar Almas"
+
+# # print(name[0])
+
+# # name[0] = "A"
+
+# print(hex(id(name[0])))
+# print(hex(id(name[1])))
+# print(hex(id(name[2])))
+# print(hex(id(name[3])))
+
+# # print(numbers)
+
+# # numbers = [1,2,3]
+
+# # print(id(numbers[0]))
+# # print(id(numbers[1]))
+# # print(id(numbers[2]))
+
+# s1 = "A"
+# s2 = "B"
+
+# print(id(1))
+# print(id(2))
+
+# y: str = 112
+
+# print(type(y))
+
