fb0da3f99009dd7c129a9e24e05b5d37cd9a1efd
Samar Almas <samaralmas8@gmail.com>
2022-03-16 19:58:30 +0500

    Lecture 3

--- lecture_2.py
+++ lecture_2.py
@@ -8,6 +8,7 @@
 # print(hex(id(name[3])))
 
 # # print(numbers)
+# print(name[-1::-1])
 
 # # numbers = [1,2,3]
 
@@ -25,3 +26,21 @@
 
 # print(type(y))
 
+# name = "Almas"
+
+# msg = "From rizwan@gmail.com.pk Tue 15 March 2022 07:43:00"
+
+# domain = msg.split(" ")
+
+# domain_name = msg.split("@")[1].split(" ")[0]
+# print(domain_name)
+
+# start = msg.find("@")+1
+#      end = msg.find(" ", start)
+# d   omain = msg[start:end]
+# print(domain)
+
+a = "dryttheaterdghfrewexfcvytythhybrswzdxfgc"
+
+first = a.find("r")
+print(first)
