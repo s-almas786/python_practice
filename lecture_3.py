74ab9ae24082ae5db49322c537ebfb7665f9a5fa
Samar Almas <samaralmas8@gmail.com>
2022-03-24 16:59:28 +0500

    some new files

---
+++ lecture_3.py
@@ -0,0 +1,32 @@
+# names = ["usman", "bilal", "ali"]
+# names.sort(key=lambda name: ord(name[0]))
+# names.reverse()
+
+# print(names)
+
+# from array import array as a
+
+# new_array = a("b", [1, 2, 3, 4])
+
+# print(new_array)
+
+# # for item in new_array:
+# #     print(item)
+
+# print(type(new_array))
+
+# num = list(range(5, 0, -1))
+# print(num)
+# s1 = ()
+# print(type(s1))
+
+s1 = {1, 2, 3}
+
+a = ['foo', 'bar', 'baz', 'qux']
+b = ['foo', 'bar', 'baz', 'qux']
+
+print(s1
+
+
+
+      )
