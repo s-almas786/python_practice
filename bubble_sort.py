83b2f5b5dca0091fe2e934c5ecd0f88f964a6fcc
Samar Almas <samaralmas8@gmail.com>
2022-03-30 19:14:11 +0500

    New file

--- bubble_sort.py
+++ bubble_sort.py
@@ -38,11 +38,11 @@
 # new_users = [user for user in users if user.endswith("ab")]
 # print(new_users)
 
-matrix = [
-    [1, 2, 3, 4],
-    [3, 4, 5, 8],
-    [7, 66, 43, 33, 90, 22]
-]
+# matrix = [
+#     [1, 2, 3, 4],
+#     [3, 4, 5, 8],
+#     [7, 66, 43, 33, 90, 22]
+# ]
 
 # new_arr = []
 # for row in matrix:
@@ -66,5 +66,37 @@ matrix = [
 #         transpose_row.append(row[i])
 #     transpose.append(transpose_row)
 
-transpose = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
-print(transpose)
+# transpose = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+# # print(transpose)
+
+# n = [[value+1 for value in row] for row in matrix]
+# print(n)
+
+# {key, value for item in iterable}
+
+prices = {
+    "laptop": 10000,
+    "mobile": 5000,
+    "power": -999,
+}
+# new_dic = {value: key for key, value in prices.items()}
+
+# for key, value in dict
+
+# new_dic = {}
+
+# for i in range(3):
+#     new_dic[i] = "Usama"
+
+# print(new_dic)
+
+# new_dic = {i: "Usama" for i in range(3)}
+# print(new_dic)
+
+# new_prices = {k: v*2 for k, v in prices.items()}
+# print(new_prices)
+
+# prices_five = {k: 0 if v < 0 else v for k, v in prices.items()}
+# print(prices_five)
+# prices_five = {k: v for k, v in prices.items() if v > 5000}
+# print(prices_five)
